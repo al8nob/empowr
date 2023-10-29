8e1352666a0280f5c20d8043dd636e4a26a0f7f8
al8nob <80894734+al8nob@users.noreply.github.com>
2023-10-29 12:54:54 +0100

    Update Signup.js
    Last code update:
    
    - password key-value is changed 
       from type number , to type string.
    
    - at line 78: error message alert is added 
       when the user's typing an email address   
       already in use by another user.
    
    - at line 80: create user with email & 
       password using firebase authentication.
    
    - at line 86: adding new document contains
       the data of new user.
    
    - at line 94: redirecting the user to the login
       page.
    
    - at line 95: success message alert displayd 
       after successful signed up.
    
    - at line 98: handling the errors of the 
       signing up.
    
    - at line 258: remove the Number from the 
       new value of the input.

--- src/components/Authentication/Signup.js
+++ src/components/Authentication/Signup.js
@@ -1,56 +1,250 @@
-import React, { createContext, useEffect, useContext, useState } from "react";
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { auth } from "../../firebase-config/firebase";
+import { createUserWithEmailAndPassword } from "firebase/auth";
+import { NavLink } from "react-router-dom";
+import { Button } from "react-bootstrap";
+import "./Signup.css";
+import { toast } from "react-toastify";
+import { useUsersContext } from "../../context/usersDbContext";
+import { db } from "../../firebase-config/firebase";
+import { getDocs, addDoc, collection, where, query } from "firebase/firestore";
 
-// our database
-import { db } from "../firebase-config/firebase";
+// when users signing up will notify them ( successful signup )
+const msgAlertSuccess = (message) => {
+  return toast.success(message, {
+    position: "bottom-right",
+    autoClose: 5000,
+    hideProgressBar: false,
+    closeOnClick: true,
+    pauseOnHover: true,
+    draggable: true,
+    progress: undefined,
+    theme: "colored",
+  });
+};
 
-// getDocs() , used to retrieve all documents in a collection
-// collection() , used to create a reference to a collection.
-import { getDocs, collection } from "firebase/firestore";
+// when users signing up will notify them ( error signup )
+const msgAlertError = (message) => {
+  return toast.error(message, {
+    position: "bottom-right",
+    autoClose: 5000,
+    hideProgressBar: false,
+    closeOnClick: true,
+    pauseOnHover: true,
+    draggable: true,
+    progress: undefined,
+    theme: "colored",
+  });
+};
 
-const UsersFirestoreContext = createContext();
+const Signup = () => {
+  // storing the data of new user that want to sign up
+  const [newUserData, setNewUserData] = useState({
+    firstName: "",
+    lastName: "",
+    email: "",
+    password: "",
+  });
 
-const UsersDbProvider = ({ children }) => {
-  // state to keep track the list of users
-  const [userList, setUserList] = useState([]);
+  // userList => all documents of users
+  // setUserList => to update the collection of documents
+  // usersCollectionRef => to access to a users collection
+  // getUsersList => getting all the users
+  const { userList } = useUsersContext();
+  console.log(userList);
 
-  // our collection reference
-  const usersCollectionRef = collection(db, "users");
+  // when users signing up will redirect them to login page.
+  const redirectLogin = useNavigate();
 
-  //   this function to get the list of users from our database
-  const getUsersList = async () => {
+  // reference users collection
+  const dbUsersRef = collection(db, "users");
+
+  const signUpFunc = async (e) => {
+    // to prevent the reload page
+    e.preventDefault();
+
+    // send request where the email fields that matches the newUserData.email
+    const matchEmail = query(
+      dbUsersRef,
+      where("email", "==", newUserData.email)
+    );
     try {
-      // we have to specify which collection we want to get all the documents from => (users)
-      const data = await getDocs(usersCollectionRef);
+      // get all documents that contains the same email field value
+      const snapshot = await getDocs(matchEmail);
 
-      // grabbing the data that we want from this response
-      // data() function in each document which you can use just to get the information such as [displayName - phoneNumber - email - etc...]
-      const filteredData = data.docs.map((doc) => ({
-        ...doc.data(),
-        uid: doc.id,
-      }));
-      setUserList(filteredData);
+      // after we got all the docs we want to return the data of each doc
+      // this array contains all the docs that have the same email field value
+      const emailMatchingArray = snapshot.docs.map((doc) => doc.data());
+
+      if (emailMatchingArray.length > 0) {
+        msgAlertError("This email address is already in use by another user.");
+      } else {
+        createUserWithEmailAndPassword(
+          auth,
+          newUserData.email,
+          newUserData.password
+        )
+          .then(async () => {
+            await addDoc(dbUsersRef, newUserData);
+            setNewUserData({
+              ...newUserData,
+              firstName: "",
+              lastName: "",
+              email: "",
+              password: "",
+            });
+            redirectLogin("/login");
+            msgAlertSuccess("Your account has been created!. Login Now");
+          })
+          .catch((error) => {
+            switch (error.code) {
+              // if the user entered an invalid email or the (uid) is already exists
+              case "auth/uid-already-exists":
+              case "auth/invalid-email":
+                return msgAlertError(
+                  "Please check your information and try again."
+                );
+
+              // if the user entered an email already in use
+              case "auth/email-already-in-use":
+                return msgAlertError(
+                  "This email address is already in use by another user."
+                );
+
+              // if the user forgot to enter his email address
+              case "auth/missing-email":
+                return msgAlertError("Enter your email address");
+
+              // if the user forgot to enter his password
+              case "auth/missing-password":
+                return msgAlertError("Enter your password");
+
+              // if the user entered a short password
+              case "auth/weak-password":
+                return msgAlertError(
+                  "Password should be at least 6 characters"
+                );
+
+              // if the user loses his connection to the internet
+              case "auth/network-request-failed":
+                return msgAlertError(
+                  "Check your internet connection and try again."
+                );
+
+              // default case, if an unknown error occurred
+              default:
+                return msgAlertError(error.code);
+            }
+          });
+      }
     } catch (err) {
-      console.log(err.code);
+      console.log(err);
     }
   };
 
-  // once the component is rendered will getting the list of users from the getUsersList function
-  useEffect(() => {
-    // this function to get the list of users
-    getUsersList();
-  }, []);
-
   return (
-    <UsersFirestoreContext.Provider
-      value={{ userList, setUserList, usersCollectionRef, getUsersList }}
-    >
-      {children}
-    </UsersFirestoreContext.Provider>
+    <>
+      <div className="signup-container">
+        <h3 className="signup-title">Sign up</h3>
+        <p className="signup-description">
+          Please fill in the information below
+        </p>
+
+        <form onSubmit={signUpFunc} className="inpFields">
+          <div className="inp-container">
+            <div className="firstLast-Name">
+              <input
+                type="text"
+                name="fname"
+                value={newUserData.firstName}
+                onChange={(e) =>
+                  setNewUserData({ ...newUserData, firstName: e.target.value })
+                }
+                className="inpField"
+                placeholder="First name"
+                autoComplete="on"
+              />
+              <input
+                type="text"
+                name="lname"
+                value={newUserData.lastName}
+                onChange={(e) =>
+                  setNewUserData({ ...newUserData, lastName: e.target.value })
+                }
+                className="inpField"
+                placeholder="Last name"
+                autoComplete="on"
+              />
+            </div>
+            <input
+              type="email"
+              name="email"
+              value={newUserData.email}
+              onChange={(e) =>
+                setNewUserData({ ...newUserData, email: e.target.value })
+              }
+              className="inpField"
+              placeholder="Email"
+              autoComplete="on"
+            />
+            <input
+              type="password"
+              name="password"
+              value={newUserData.password}
+              onChange={(e) =>
+                setNewUserData({
+                  ...newUserData,
+                  password: e.target.value,
+                })
+              }
+              className="inpField"
+              placeholder="Password"
+              autoComplete="new-password"
+              min="6"
+              max="12"
+            />
+          </div>
+          <Button type="submit" className="submitDataBtn" variant="warning">
+            SIGN UP
+          </Button>
+        </form>
+
+        <p className="TOS-PP-NOTE">By creating an account you agree with our</p>
+        <div className="TOS-PP">
+          <NavLink to="/" className="TOS">
+            Terms of Service
+          </NavLink>{" "}
+          &{" "}
+          <NavLink to="/" className="PP">
+            Privacy Policy.
+          </NavLink>
+        </div>
+
+        <p className="noteForm">
+          Already have an account?{" "}
+          <NavLink to="/login" className="loginLink">
+            Login
+          </NavLink>
+        </p>
+      </div>
+      <div className="d-flex flex-column gap-2 align-items-center text-center">
+        {userList.map((user, i) => {
+          return (
+            <div key={i}>
+              <h2>
+                {user.firstName.toUpperCase() +
+                  " " +
+                  user.lastName.toUpperCase()}
+              </h2>
+              <p>{user.email}</p>
+              <small>{user.uid}</small>
+            </div>
+          );
+        })}
+      </div>
+    </>
   );
 };
 
-const useUsersContext = () => {
-  return useContext(UsersFirestoreContext);
-};
-
-export { UsersDbProvider, useUsersContext };
+export default Signup;
