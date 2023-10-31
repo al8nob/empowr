d4df7a600708b1a9350d03303ea2cea8c060d79d
al8nob <80894734+al8nob@users.noreply.github.com>
2023-10-31 21:25:54 +0100

    Update Login.js

--- src/components/Authentication/Login.js
+++ src/components/Authentication/Login.js
@@ -6,7 +6,7 @@ import { Button } from "react-bootstrap";
 import { toast } from "react-toastify";
 import "./Login.css";
 import { db } from "../../firebase-config/firebase";
-import { getDocs, query, where, collection } from "firebase/firestore";
+import { getDocs, query, where, collection, addDoc } from "firebase/firestore";
 
 // sm icons
 import googleIcon from "../../images/googleIcon.png";
@@ -53,17 +53,44 @@ const Login = () => {
   // when the users is logged in successfully will redirect them to their profile.
   const redirectProfile = useNavigate();
 
+  /*
+  
+  1st step: sign in with google account (done)
+  2nd step: storing the results inside a constant (done)
+  3rd step: check if the data such as [email, displayName, phoneNumber, etc...] exists in the database
+  4th step: if exist => getting email and displayName
+  5th step: if not exist => add a new document inside users collection contains the email and displayName
+  
+  */
+
   const signInWithGoogle = () => {
+    // sign in with google account
     signInWithPopup(auth, googleProvider)
-      .then((userCredential) => {
-        const userInfo = userCredential;
-        console.log(userInfo);
-        redirectProfile("/profile");
-        msgAlertSuccess("Welcome, enjoy your shopping experience.");
+      .then(async (result) => {
+        // storing the results inside a constant
+        const user = result.user;
+        const matchEmail = query(dbUsersRef, where("email", "==", user.email));
+
+        try {
+          const snapshot = await getDocs(matchEmail);
+          const emailExist = snapshot.docs.map((doc) => doc.data());
+          console.log(emailExist);
+          if (emailExist.length > 0) {
+            console.log(user.email, " ", "email exist in database");
+          } else {
+            console.log("email's not exist in database");
+            await addDoc(dbUsersRef, {
+              email: user.email,
+              displayName: user.displayName,
+            });
+            console.log("it's exist now after signing in");
+          }
+        } catch (error) {
+          console.log(error.code);
+        }
       })
       .catch((error) => {
-        console.log(error);
-        msgAlertError("Please check your email and password and try again.");
+        console.log(error.code);
       });
   };
 
@@ -131,49 +158,17 @@ const Login = () => {
     }
   };
 
-  // const submitForm = (e) => {
-  //   e.preventDefault();
-  //   signInWithEmailAndPassword(auth, userData.email, userData.password)
+  // const signInWithGoogle = () => {
+  //   signInWithPopup(auth, googleProvider)
   //     .then((userCredential) => {
   //       const userInfo = userCredential;
   //       console.log(userInfo);
   //       redirectProfile("/profile");
   //       msgAlertSuccess("Welcome, enjoy your shopping experience.");
-  //       setUserData({ ...userData, email: "", password: "" });
   //     })
   //     .catch((error) => {
-  //       // if any of the next cases happened from the user will display a message to inform him
-  //       switch (error.code) {
-  //         // if the user entered invalid data such as email - password , or the user in not found
-  //         case "auth/invalid-email":
-  //         case "auth/invalid-password":
-  //         case "auth/invalid-login-credentials":
-  //           return msgAlertError(
-  //             "Please check your email and password and try again."
-  //           );
-  //         // if the user hasn't an account
-  //         case "auth/user-not-found":
-  //           return msgAlertError(
-  //             "We couldn't find an account with that email address."
-  //           );
-
-  //         // if the user forgot to enter his email address
-  //         case "auth/missing-email":
-  //           return msgAlertError("Enter your email address");
-
-  //         // if the user forgot to enter his password
-  //         case "auth/missing-password":
-  //           return msgAlertError("Enter your password");
-
-  //         // if the user loses his connection to the internet
-  //         case "auth/network-request-failed":
-  //           return msgAlertError(
-  //             "Check your internet connection and try again."
-  //           );
-  //         // default case, if an unknown error occurred
-  //         default:
-  //           return msgAlertError(error.code);
-  //       }
+  //       console.log(error);
+  //       msgAlertError("Please check your email and password and try again.");
   //     });
   // };
 
